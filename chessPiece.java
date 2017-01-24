066ccbdfd7ab0fa45fa99ddd94d423aa71a0eb5e
tanessav <vanessatanv@gmail.com>
2017-01-24 15:48:48 -0800

    Add files via upload

---
+++ chessPiece.java
@@ -0,0 +1,13 @@
+package chessPiece;
+
+public class chessPiece {
+	int col;
+	int row;
+	boolean isAttacking;
+	
+	class King extends chessPiece {
+		boolean isAttacking (chessPiece c) {
+			
+		}
+	}
+}
