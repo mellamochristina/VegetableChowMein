8f0e439ec65e9ba8161741897d4540d940118011
mellamochristina <christinavo2509@yahoo.com>
2017-01-25 20:33:23 -0800

    added some pseudocode methods

--- chessPiece.java
+++ chessPiece.java
@@ -1,16 +1,29 @@
 package chessPiece;
 
+
 public class chessPiece {
 	int col;
 	int row;
-	boolean color;
 	boolean isAttacking (chessPiece c) {
  	//check if c is at (col,row)
  	}
-	
-	class King extends chessPiece {
-		boolean isAttacking (chessPiece c) {
-			
+	//prints all values stored in linked list...pseudocode
+	public void traverse(){
+		Node current = head;
+		while (current!= null){
+			print(current.value);
+			current = current.next;
 		}
+	}
+	//returns true if query is found in the list and returns false otherwise
+	boolean find(){
+		//traverse
+		//use a for loop. check if current instance x[i]==query
+	}
+	//inserts in a new value at the front of a linked list...pseudocode
+	public void insert(int newValue){
+		Node latest = new Node(newVal);
+		latest.next = head;
+		head = latest;
 	}
 }
