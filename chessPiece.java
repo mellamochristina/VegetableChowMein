3e3f686f16120600bcf5899fc48515245e82c439
tanessav <vanessatanv@gmail.com>
2017-01-26 14:41:15 -0800

    Update chessPiece.java

--- chessPiece.java
+++ chessPiece.java
@@ -3,46 +3,47 @@ package chessPiece;
 public class chessPiece {
 	int col;
 	int row;
+	char color; 
 	boolean isAttacking (chessPiece c) {
 		//check to make sure that two pieces are not in the same square
-	}
-	boolean findPiece () {
-		//find what piece is at given square
-		//read in every 2nd input line using scanner
+		if (c.col == col || c.row == row) {
+			return false;
+		}
+		return true;
 	}
 	boolean verifySolution (chessPiece [] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			for (int j = 0; j < pieces.length; j++) {
-				if pieces[i].isAttacking(pieces[j]) {
+				if (pieces[i].isAttacking(pieces[j])) {
 					return false;
 				}
 				return true;
 			}
 		}
 	}
-	class King extends chessPiece {
-		boolean isAttacking (chessPiece c) {
-			//king can attack 1 square in any direction
-		}
+}
+class King extends chessPiece {
+	boolean isAttacking (chessPiece c) {
+		//king can attack 1 square in any direction
 	}
-	class Queen extends chessPiece {
-		boolean isAttacking (chessPiece c) {
-			//queen can attack up,down,left,right,and diagonal
-		}
+}
+class Queen extends chessPiece {
+	boolean isAttacking (chessPiece c) {
+		//queen can attack up,down,left,right,and diagonal
 	}
-	class Rook extends chessPiece {
-		boolean isAttacking (chessPiece c) {
-			//rook can attack up,down,left, and right
-		}
+}
+class Rook extends chessPiece {
+	boolean isAttacking (chessPiece c) {
+		//rook can attack up,down,left, and right
 	}
-	class Bishop extends chessPiece {
-		boolean isAttacking (chessPiece c) {
-			//bishop can attack diagonally
-		}
+}
+class Bishop extends chessPiece {
+	boolean isAttacking (chessPiece c) {
+		//bishop can attack diagonally
 	}
-	class Knight extends chessPiece {
-		boolean isAttacking (chessPiece c) {
-			//knight can attack L-shape
-		}
+}
+class Knight extends chessPiece {
+	boolean isAttacking (chessPiece c) {
+		//knight can attack L-shape
 	}
 }
