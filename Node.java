4b774e7f5902665f95701896e5d0380907d2b160
mellamochristina <christinavo2509@yahoo.com>
2017-01-28 21:21:20 -0800

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,17 @@
+public class Node {
+	int val;
+	Node next;
+	
+	public int getVal() {
+		return val;
+	}
+	public void setVal(int val) {
+		this.val = val;
+	}
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
