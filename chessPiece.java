e09a17342c4baf0e5be44e04a1d88a898dc4d90d
mellamochristina <christinavo2509@yahoo.com>
2017-01-29 15:12:43 -0800

    Update chessPiece.java

--- chessPiece.java
+++ chessPiece.java
@@ -31,10 +31,17 @@ class Queen extends ChessPiece {
 	Queen (char piece, int col, int row) {
 		super(piece, col, row);
 	}
-	boolean isAttacking (ChessPiece c) {
+	boolean isAttacking (ChessPiece c) { //from HW1 solution
 		//queen can attack up,down,left,right,and diagonal
-		return false;
+	if (row ==c.row || col == c.col) // if piece has same row or column as c, piece is attacking c
+            return true;
+	// if piece is on same diagonal as c, this is attack. we use absolute values to determine diagonal
+        else if (Math.abs(row-c.row) == Math.abs(col - c.col)) 
+            return true;
+        else
+            return false; // piece is not attacking c
 	}
+	
 }
 
 class Rook extends ChessPiece {
@@ -43,7 +50,10 @@ class Rook extends ChessPiece {
 	}
 	boolean isAttacking (ChessPiece c) {
 		//rook can attack up,down,left, and right
-		return false;
+		if (row==c.row||col==c.col) //if piece has same row or column as c, piece is attacking c
+			return true;
+		else
+		return false; 
 	}
 }
 
@@ -53,6 +63,9 @@ class Bishop extends ChessPiece {
 	}
 	boolean isAttacking (ChessPiece c) {
 		//bishop can attack diagonally
+		if (Math.abs(row-c.row) == Math.abs(col - c.col)) //if piece is on same diagonal as c, it attacks c
+			return true;
+		else
 		return false;
 	}
 }
