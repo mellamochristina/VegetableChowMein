5bbe722e1d05941d01858482cf8706d51c9e8589
mellamochristina <christinavo2509@yahoo.com>
2017-01-31 01:16:23 -0800

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -2,41 +2,42 @@ package chessPiece;
 
 public class LinkedList{
 	  Node head;
-	  int count;
+	  int size; //used for the obtaining size of the linked list
 	  
 	  public LinkedList(){
-		    //add ChessPiece to linked list
+		    //empty constructor prevents java error
 		  }
 	  
+	//method for retrieving a piece at an instance
 	  public ChessPiece get(int i){
-	    Node current = head;
+	    Node current = head; //set the head of the linked list to current
 	    int n = 0;
-	    while(current.next !=null && n<i){
-	      current = current.getNext();
-	      n++;
+	    while(current.next !=null && n<i){ //makes sure it doesn't return a NullPointerException
+	      current = current.getNext(); //sets the next value in linked list to the current
+	      n++; 
 	    }
-	    return current.getVal();
+	    return current.getVal(); //returns the piece at that instance
 	  }
-	  
+	  //method for adding a piece into linked list
 	  public void add(ChessPiece value){
-		    if (head==null){
+		    if (head==null){ //if list is empty, the piece becomes the head of the list
 		      head = new Node(value);
 		    }
 		    else {
-			    Node temp = new Node(value);
-			    Node current = head;
+			    Node temp = new Node(value); //stores the piece into a temporary variable
+			    Node current = head; //the current piece becomes the head of list
 			    if(current != null){
-			      while (current.getNext()!=null){
-			        current = current.getNext();
+			      while (current.getNext()!=null){ //makes sure it's not grabbing past the linked list
+			        current = current.getNext(); //cuurent becomes the next instance
 			      }
-			      current.setNext(temp);
+			      current.setNext(temp); //next current value is now empty, so temp can now be next current
 			    }
 		    }
-		    count++;
+		    size++; //size increments with each addition of a piece
 		  }
-	  
+	  //method to obtain size of list
 	  public int size(){
-		    return count;
+		    return size;
 		  }
 	  
 	}
