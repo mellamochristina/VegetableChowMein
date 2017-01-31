a6c05db1a258b1354424fd3f7aad59a74c7a4792
mellamochristina <christinavo2509@yahoo.com>
2017-01-31 01:21:50 -0800

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,5 +1,7 @@
-package chessPiece;
+import java.util.*;
+import java.io.*;
 
+//code obtained with help from https://github.com/vigilkj/LinkedList
 public class Node {
 	  ChessPiece value;
 	  Node next;
@@ -8,16 +10,19 @@ public class Node {
 	    value = val;
 	    next = null;
 	  }
-	  
+	  //gets piece
 	  public ChessPiece getVal() {
 	    return value;
 	  }
+	//sets piece
 	  public void setVal(ChessPiece val) {
 	    value = val;
 	  }
+	//gets next piece
 	  public Node getNext() {
 	    return next;
 	  }
+	//sets next piece
 	  public void setNext(Node next) {
 	    this.next = next;
 	  }
