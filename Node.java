05d0322e39cd5db81dcb5fe74f5a60618a679106
mellamochristina <christinavo2509@yahoo.com>
2017-01-30 22:22:36 -0800

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,17 +1,25 @@
+import java.util.*;
+import java.io.*;
+
 public class Node {
-	int val;
-	Node next;
-	
-	public int getVal() {
-		return val;
-	}
-	public void setVal(int val) {
-		this.val = val;
-	}
-	public Node getNext() {
-		return next;
-	}
-	public void setNext(Node next) {
-		this.next = next;
-	}
+  ChessPiece value;
+  Node next;
+  
+  public Node(ChessPiece val){
+    value = val;
+    next = null;
+  }
+  
+  public ChessPiece getVal() {
+    return value;
+  }
+  public void setVal(ChessPiece val) {
+    value = val;
+  }
+  public Node getNext() {
+    return next;
+  }
+  public void setNext(Node next) {
+    this.next = next;
+  }
 }
